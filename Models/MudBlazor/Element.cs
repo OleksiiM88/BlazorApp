eab0f9c5ddd6ed7ec7ce7030f9541a15c01c0022
Oleksii Maievskyi <oleksii.maievskyi@sulzeriot.onmicrosoft.com>
2022-10-01 16:41:28 +0300

    MudBalzor table added

---
+++ Models/MudBlazor/Element.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorApp.Models.MudBlazor
+{
+    public class Element
+    {
+        public Guid Id { get; set; }
+
+        public int Number { get; set; }
+
+        public string? ContactName { get; set; }
+
+        public string? Sign { get; set; }
+
+        public float? Price { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Email { get; set; }
+
+        public DateTime Registered { get; set; }
+    }
+}
